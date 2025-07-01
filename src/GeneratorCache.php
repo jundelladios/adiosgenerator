1f3ea9280e155d14c76db65e73d9bc2ba2b9ba50
jundellagbo <jundellagbo10dd@gmail.com>
2025-07-02 03:16:44 +0800

    divi clear cache enhancements

--- src/GeneratorCache.php
+++ src/GeneratorCache.php
@@ -38,10 +38,9 @@ class GeneratorCache {
 
   public static function clear_cache() {
     if(class_exists("ET_Core_PageResource")) {
-      \ET_Core_PageResource::do_remove_static_resources( 'all', 'all' );
-      \ET_Core_PageResource::do_remove_static_resources( 'all', 'all', false, 'dynamic' );
-      \ET_Core_PageResource::do_remove_static_resources( 'all', 'all', true );
-      // ensure divi assets propagate the new url.
+      \ET_Core_PageResource::remove_static_resources( "all", "all" );
+      et_core_clear_transients();
+      et_core_clear_wp_cache();
     }
     do_action( 'breeze_clear_all_cache' );
     wp_cache_flush();
